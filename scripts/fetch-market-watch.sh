7dae6774e010c1fb446304bed448493a8bc2a091
lance-leverfi <lance@leverfi.io>
2025-02-28 15:49:50 +0800

    chore: add echo for cron logs

--- scripts/fetch-market-watch.sh
+++ scripts/fetch-market-watch.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
+echo "Cron automated fetching from market watch"
 
 # Define the API URL
 API_URL="https://dbapi.allview-parabolea.com/earnings-calendar/market-watch/scrape"
 
 # Make the request
 curl -X POST "$API_URL"
-
-# If authentication is required, use:
-# curl -X GET -H "Authorization: Bearer YOUR_TOKEN" "$API_URL"
